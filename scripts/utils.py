3d3abf771601f0e1aa022359d5fa9b64132006ae
dagmaros27 <dagxx27@gmail.com>
2025-06-25 15:27:04 +0300

    feat: finetune ner model

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,15 +10,26 @@ def clean_amharic_text(text: str) -> str:
     if pd.isnull(text):
         return ""
 
-    text = text.replace("፦", " ")
-    text = re.sub(r'(?<!\s)(09\d{8})(?!\s)', r' \1 ', text)
-    
-    text = re.sub(fr'([\u1200-\u137F])(\d)', r'\1 \2', text)
-    text = re.sub(fr'(\d)([\u1200-\u137F])', r'\1 \2', text)
-    text = re.sub(r'[^\w\s፡።፣፤፥፦መሀቀለዐነአሰቀቸበኸተሰከወፈ]+', '', text)
+    text = re.sub(r"[\U00010000-\U0010FFFF]", " ", text)
+
+    text = re.sub(r"(ዋጋ)\s*[:፦\-：]*\s*", r"\1 ", text)
+
+    text = re.sub(r"(?<=\d)(?=\s?(ብር|birr|etb))", " ", text, flags=re.IGNORECASE)
+
+    text = re.sub(r'(?<=[\u1200-\u137F])(?=\d)', ' ', text)
+    text = re.sub(r'(?<=\d)(?=[\u1200-\u137F])', ' ', text)
+    text = re.sub(r'(?<=[\u1200-\u137F])(?=[a-zA-Z])', ' ', text)
+    text = re.sub(r'(?<=[a-zA-Z])(?=[\u1200-\u137F])', ' ', text)
+
+
+    phone_pattern = r'(\+251[79]\d{8}|0[79]\d{8})'
+    text = re.sub(fr'(?<!\s){phone_pattern}(?!\s)', r' \1 ', text)
 
     text = re.sub(r'https?://\S+', '', text)
     text = re.sub(r'@\w+', '', text)
+
+    text = re.sub(r'[^a-zA-Z0-9\u1200-\u137F\s፡።፣፤፥፦መሀቀለዐነአሰቀቸበኸተሰከወፈ]+', '', text)
+    
 
     text = re.sub(r'\s+', ' ', text).strip()
 
